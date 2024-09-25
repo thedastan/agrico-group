208a640318ced44964b003f15ceba1b386a7703a
Dastan Mukeev <55836145+thedastan@users.noreply.github.com>
2024-09-25 22:28:42 +0600

    Update admin.ts

--- src/constants/admin.ts
+++ src/constants/admin.ts
@@ -1,11 +1,11 @@
-export const PHONE_NUMBER = '+996332123456'
+export const PHONE_NUMBER = '+996221210007'
 
 export const WHATSAPP_NUMBER = PHONE_NUMBER
 
-export const EMAIL_ADDRESS = 'agrico@gmail.com'
+export const EMAIL_ADDRESS = 'info@agrico-group.org'
 export const INSTAGRAM = 'agrico'
 
-export const LOCATION = 'Бишкек, Улица Ибраимова, 64/2'
+export const LOCATION = 'Бишкек, Улица Раззакова 32'
 
 /// links
 
@@ -17,8 +17,8 @@ export const INSTAGRAM_LINK = `https://instagram.com/${INSTAGRAM}`
 export const FACEBOOK_LINK = ``
 export const YOUTUBE_LINK = ``
 
-export const TELEGRAM_LINK = `https://t.me/+996705045005`
+export const TELEGRAM_LINK = `https://t.me/+996221210007`
 
-export const LOCATION_LINK = 'https://go.2gis.com/hawqt'
+export const LOCATION_LINK = 'https://go.2gis.com/1ojrg'
 
 export const MOTION_WEB_LINK = 'https://www.motionweb.kg/'
